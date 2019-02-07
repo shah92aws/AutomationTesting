e2261fd56059ed6bc64ff46b59d39d7ef14be891
Shah Ridzuan <shahridzuan.aws@gmail.com>
2019-02-08 00:25:25 +0800

    add 9 search places

--- AutomationTestingExercise1/src/com/testdata/dataproviders/DataProviders1.java
+++ AutomationTestingExercise1/src/com/testdata/dataproviders/DataProviders1.java
@@ -6,11 +6,17 @@ public class DataProviders1 {
 	
 	@DataProvider(name="DataSet1")
 	public Object[][] DataProviders1() {
-		Object[][] data = new Object[3][1];
+		Object[][] data = new Object[9][1];
 		
 		data[0][0] = "Kuala Lumpur";
 		data[1][0] = "Selangor";
 		data[2][0] = "Kelantan";
+		data[3][0] = "Johor";
+		data[4][0] = "Pahang";
+		data[5][0] = "Perlis";
+		data[6][0] = "Perak";
+		data[7][0] = "Negeri Sembilan";
+		data[8][0] = "Melaka";
 		
 		return data;
 	}
